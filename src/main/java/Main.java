893c6125c4284ea6462ef2978ddd9eec8a0a411a
Adrian Danilescu <hiraky5@gmail.com>
2017-11-15 15:33:52 +0100

    50%

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,15 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        int verify = -1; // campaign => verify = 0; if banner => verify = 1;
+        int id=0;
+        get("/campaign/:id", (req, res) -> {
+            return "Campaign ID is: "+ req.params(":id");
+        });
+        get("/banner/:id", (req, res) -> {
+            return "Banner ID is: "+ req.params(":id");
+        });
+        System.out.println(verify);
+        System.out.println(id);
     }
 }
