bd344d34c87316f5d80ec17f6be9565c114cc8b6
ersanebit <ersan.ebit@gmail.com>
2017-11-15 14:51:28 +0100

    The objects have been created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello");
+        System.out.println("hy");
     }
 }
