00a84deaa334cae7aef54ab1265e587fc12cb1f7
Adrian Danilescu <hiraky5@gmail.com>
2017-11-15 15:11:43 +0100

    Revert "Fix"
    This reverts commit 31e5b2635b9f7625f1e97ae0b2ee6c720cb60272.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,14 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-
+<<<<<<< HEAD
+        get("/hello", (req, res) -> "Hello");
+        System.out.println("hy");
+=======
         get("/campaign/:id", (req, res) ->{
-            return "Hello";
+            return "";
         });
+
+>>>>>>> origin/master
     }
 }
